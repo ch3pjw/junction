5a17aa1c01d437a0615576c87eb5e2e77bab44a8
Paul Weaver <paul@ruthorn.co.uk>
2013-12-14 22:41:51 +0000

    Updated zebra example to show Dad more stuff

--- examples/zebra.py
+++ examples/zebra.py
@@ -2,7 +2,9 @@ from junction import Root, Text, Zebra, Terminal
 
 text1 = Text('Some interesting text might go here')
 text2 = Text('The Zebra will help you differentiate lines')
-content = [text1, text2] * 10
+text3 = Text('A third line will help the logic become clearer')
+text3.min_height = 2
+content = [text1, text2, text3] * 10
 term = Terminal()
 zebra = Zebra(*content, odd_format=term.on_color(235))
 root = Root(zebra)
