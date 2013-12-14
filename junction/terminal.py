38d92157adfc13212fe7729279e8b071aab41feb
Paul Weaver <paul@ruthorn.co.uk>
2013-12-14 22:40:47 +0000

    Ported functionality for tracking term state and enabling SIGTSTP...
    from pyamp.

--- junction/terminal.py
+++ junction/terminal.py
@@ -1,12 +1,97 @@
 import blessings
+import termios
+import signal
+import os
+from functools import wraps
 
 from .formatting import Format, StringWithFormatting
+
+
+def _override_sugar(func):
+    '''Use this decorator to override an attribute that is specified in
+    blessings' sugar dict with your own function that adds some additional
+    functionality.
+    '''
+    attr_name = func.__name__
+    @property
+    @wraps(func)
+    def func_that_uses_terminal_sugar(self):
+        func(self)
+        return self.__getattr__(attr_name)
+    return func_that_uses_terminal_sugar
 
 
 class Terminal(blessings.Terminal):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._normal = super()._resolve_formatter('normal')
+        # We track these to make SIGTSTP restore the terminal correctly:
+        self._is_fullscreen = False
+        self._has_hidden_cursor = False
+        self._resolved_sugar_cache = {}
+
+    def __getattr__(self, attr):
+        '''We override ___getattr__ so that we don't do blessings' annoying
+        caching by attribute-setting side-effect! This means we can override
+        sugar without fear! See the _override_sugar decorator.
+        '''
+        try:
+            return self._resolved_sugar_cache[attr]
+        except KeyError:
+            if self._does_styling:
+                resolution = self._resolve_formatter(attr)
+            else:
+                resolution = blessings.NullCallableString()
+            self._resolved_sugar_cache[attr] = resolution
+            return resolution
+
+    @_override_sugar
+    def enter_fullscreen(self):
+        self._is_fullscreen = True
+
+    @_override_sugar
+    def exit_fullscreen(self):
+        self._is_fullscreen = False
+
+    @_override_sugar
+    def hide_cursor(self):
+        self._has_hidden_cursor = True
+
+    @_override_sugar
+    def normal_cursor(self):
+        self._has_hidden_cursor = False
+
+    def handle_sigtstp(self, sig_num, stack_frame):
+        # Store current state:
+        if self.is_a_tty:
+            cur_tty_attrs = termios.tcgetattr(self.stream)
+            termios.tcsetattr(
+                self.stream, termios.TCSADRAIN, self._orig_tty_attrs)
+        is_fullscreen = self._is_fullscreen
+        has_hidden_cursor = self._has_hidden_cursor
+        # Restore normal terminal state:
+        if is_fullscreen:
+            self.stream.write(self.exit_fullscreen)
+        if has_hidden_cursor:
+            self.stream.write(self.normal_cursor)
+        self.stream.flush()
+        # Unfortunately, we have to remove our signal handler and
+        # reinstantiate it after we're continued, because the only way we
+        # can get python to sleep is if we send the signal to ourselves again
+        # with no handler :-(
+        current_handler = signal.signal(signal.SIGTSTP, signal.SIG_DFL)
+        def restore_on_sigcont(sig_num, stack_frame):
+            signal.signal(signal.SIGTSTP, current_handler)
+            signal.signal(signal.SIGCONT, signal.SIG_DFL)
+            if self.is_a_tty:
+                termios.tcsetattr(
+                    self.stream, termios.TCSADRAIN, cur_tty_attrs)
+            if is_fullscreen:
+                self.stream.write(self.enter_fullscreen)
+            if has_hidden_cursor:
+                self.stream.write(self.hide_cursor)
+        signal.signal(signal.SIGCONT, restore_on_sigcont)
+        os.kill(os.getpid(), signal.SIGTSTP)
 
     def draw_block(self, block, x, y, normal=None):
         if normal is not None:
--- test/test_terminal.py
+++ test/test_terminal.py
@@ -1,5 +1,9 @@
 from unittest import TestCase
+from mock import patch
+
 import blessings
+import signal
+import os
 from io import StringIO
 
 from junction import Terminal
@@ -7,6 +11,61 @@ from junction.terminal import get_terminal
 
 
 class TestTerminal(TestCase):
+    def test_overridden_sugar(self):
+        junction_terminal = Terminal(force_styling=True)
+        blessings_terminal = blessings.Terminal(force_styling=True)
+        for attr_name in (
+                'enter_fullscreen', 'exit_fullscreen', 'hide_cursor',
+                'normal_cursor'):
+            # Check that we're actually behaving like a tty:
+            self.assertTrue(bool(getattr(blessings_terminal, attr_name)))
+            # Check normal retrieval
+            self.assertEqual(
+                getattr(junction_terminal, attr_name),
+                getattr(blessings_terminal, attr_name))
+            # We check retrieval a second time because we cache:
+            self.assertEqual(
+                getattr(junction_terminal, attr_name),
+                getattr(blessings_terminal, attr_name))
+
+    @patch('junction.terminal.termios', autospec=True)
+    @patch('junction.terminal.signal.signal', autospec=True)
+    @patch('junction.terminal.os.kill', autospec=True)
+    def test_handle_sigtstp(self, mock_kill, mock_signal, mock_termios):
+        # This is quite a poor unit test, in the sense that it basically
+        # checks that the code I've written is the code I've written! Any
+        # suggestions on how to test this in a less invasive way are welcome!
+        mock_termios.tcgetattr.return_value = 'Wobble'
+        mock_signal.return_value = 'current_handler'
+        fake_tty = StringIO()
+        term = Terminal(stream=fake_tty, force_styling=True)
+        term._is_fullscreen = True
+        term._has_hidden_cursor = True
+        term.is_a_tty = True
+        term.handle_sigtstp(None, None)
+        mock_termios.tcgetattr.assert_called_once_with(fake_tty)
+        self.assertTrue(bool(fake_tty.getvalue()))
+        self.assertIn(term.exit_fullscreen, fake_tty.getvalue())
+        self.assertIn(term.normal_cursor, fake_tty.getvalue())
+        mock_signal.assert_any_call(signal.SIGTSTP, signal.SIG_DFL)
+        signal_obj, cont_handler = mock_signal.call_args[0]
+        self.assertEqual(signal_obj, signal.SIGCONT)
+        mock_kill.assert_called_once_with(os.getpid(), signal.SIGTSTP)
+
+        fake_tty.truncate(0)
+        cont_handler(None, None)
+        mock_signal.assert_any_call(signal.SIGTSTP, 'current_handler')
+        mock_signal.assert_any_call(signal.SIGCONT, signal.SIG_DFL)
+        self.assertIn(term.enter_fullscreen, fake_tty.getvalue())
+        self.assertIn(term.hide_cursor, fake_tty.getvalue())
+        mock_termios.tcsetattr.assert_called_with(
+            fake_tty, mock_termios.TCSADRAIN, 'Wobble')
+
+    def test_getattr(self):
+        term = Terminal()
+        term._does_styling = False
+        self.assertIsInstance(term.blue, blessings.NullCallableString)
+
     def test_draw_block(self):
         self.maxDiff = 0  # protect from difficult terminal output on failure
         blessings_term = blessings.Terminal(force_styling=True)
