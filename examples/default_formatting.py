133a8efddda537998454ce6e732818c9b6328887
Paul Weaver <paul@ruthorn.co.uk>
2013-12-22 02:38:33 +0000

    Added an example of default formatting

---
+++ examples/default_formatting.py
@@ -0,0 +1,9 @@
+from junction import Root, Label
+
+
+label = Label(
+    'One word in the sentence has ' + Root.format.yellow('special') +
+    ' formatting, whilst the rest is determined by whatever is default.')
+label.default_format = Root.format.bright_blue
+root = Root(label)
+root.run()
