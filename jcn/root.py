90bece7c34186a7f5a6a749fa3043293608473e6
Paul Weaver <paul@ruthorn.co.uk>
2013-12-30 03:01:13 +0000

    Augment documentation for Root

--- jcn/root.py
+++ jcn/root.py
@@ -23,31 +23,46 @@ from .terminal import get_terminal, Keyboard
 
 
 class Root(ABCUIElement):
+    '''
+    :parameter element: The element that will be drawn when the root is
+        drawn. (Optional, can be reassigned at any time.)
+    :parameter styles: FIXME: A dictionary mapping names to particular Format
+        or StringWithFormatting objects. (Optional.)
+    :parameter terminal: An instnace of a :class:`Terminal` object to use
+        for communication with your TTY. (Optional, we will grab a default
+        and can be reassigned so long as we're not currently running.)
+    :parameter loop: The asyncio event loop to use for the main :meth:`run`
+        method. (Optional, we will grab a default if none is provided, and
+        the loop can be reassigned so long as we're not currently running.)
+
+    A :class:`Root` object will normally form the nucleus of your application.
+    It performs three key roles:
+
+    * Managing running the default :class:`Terminal` and the :mod:`asyncio`
+      event loop
+    * Providing easy access to add formats and styles to your applications
+      content via :attr:`format` and :attr:`style`
+    * Acting as the base of a tree of UI elements (instances of
+      :class:`ABCUIElement`)
+
+    A simple example of all three of these roles can be found in
+    ``examples/quick_brown_fox.py``:
+
+    .. literalinclude:: ../../examples/quick_brown_fox.py
+        :lines: 16-
+
+    '''
     format = FormatPlaceholderFactory()
     style = StylePlaceholderFactory()
 
     def __init__(self, element=None, styles=None, terminal=None, loop=None):
-        '''Represents the root element of a tree of UI elements. We are
-        associated with a Terminal object, so we're in the unique position of
-        knowing our own width and height constraints, and are responsible for
-        passing those down the tree when we are asked to draw ourselves.
-
-        :parameter element: The element which will be drawn when the root is
-            drawn. (Optional, can be reassigned at any time.)
-        :parameter styles: A dictionary mapping names to particular Format or
-            StringWithFormatting objects. (Optional.)
-        :parameter terminal: An instnace of a junction.Terminal object to use
-            for communication with your TTY. (Optional, we will grab a default
-            and can be reassigned so long as we're not currently running.)
-        :parameter loop: The asyncio event loop to use for the main ``run()``
-            method. (Optional, we will grab a default if none is provided, and
-            the loop can be reassigned so long as we're not currently running.)
-        '''
         super().__init__()
         self._element = None
         if element:
             self.element = element
+        # FIXME: styles will need to go
         self.styles = styles
+        # FIXME: should terminal and loop be passed in for run() only?
         self.terminal = terminal or get_terminal()
         self.loop = loop or asyncio.get_event_loop()
         self.keyboard = Keyboard()
@@ -87,6 +102,8 @@ class Root(ABCUIElement):
         self.draw()
 
     def run(self):
+        '''Run your application. FIXME: detail!
+        '''
         with self.terminal.fullscreen(), self.terminal.hidden_cursor(), (
                 self.terminal.unbuffered_input()), (
                 self.terminal.nonblocking_input()), (
@@ -100,11 +117,15 @@ class Root(ABCUIElement):
             self.loop.run_forever()
 
     def draw(self):
+        '''FIXME
+        '''
         super().draw(
             self.terminal.width, self.terminal.height, terminal=self.terminal,
             styles=self.styles)
 
     def update(self):
+        '''FIXME
+        '''
         super().update(
             self.default_format, terminal=self.terminal, styles=self.styles)
 
